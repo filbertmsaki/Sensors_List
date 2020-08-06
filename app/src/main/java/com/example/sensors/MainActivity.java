61b90e1b7a3c0704ebce31d742b032a06d5713ac
filbertmsaki <msakifil111@gmail.com>
2020-08-06 07:35:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sensors/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.sensors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SensorManager mgr;
+    private TextView txtList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mgr = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        txtList = (TextView)findViewById(R.id.sensorslist);
+        List<Sensor> sensorList = mgr.getSensorList(Sensor.TYPE_ALL);
+        StringBuilder strBuilder = new StringBuilder();
+        for(Sensor s: sensorList){
+            strBuilder.append(s.getName()+"\n");
+        }
+        txtList.setVisibility(View.VISIBLE);
+        txtList.setText(strBuilder);
+    }
+}
